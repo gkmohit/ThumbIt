ea91991dc716f013d5bd035cd44508b7fa50d996
Mohit Kishore <gkmohit@gmail.com>
2016-04-05 13:00:49 -0400

    Added home page activity and changed colours

--- app/src/main/java/com/iwadevelopers/www/thumbit/View/HomePage.java
+++ app/src/main/java/com/iwadevelopers/www/thumbit/View/HomePage.java
@@ -1,17 +1,47 @@
 package com.iwadevelopers.www.thumbit.View;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.iwadevelopers.www.thumbit.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class HomePage extends AppCompatActivity {
+
+    @Bind(R.id.firstTo100Text)
+    TextView mFirstTo100Text;
+
+    @Bind(R.id.againstTimeText)
+    TextView mAgainstTimeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        ButterKnife.bind(this);
 
-        
+
+        Typeface fontAwesome = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf"); //Fontawrsome
+        Typeface dotBold = Typeface.createFromAsset(getAssets(), "dotBold.TTF"); //Dot Bold
+        Typeface dotMatrix = Typeface.createFromAsset(getAssets(), "dotMatrix.TTF"); //Dot Matrix
+
+        mFirstTo100Text.setTypeface(dotBold);
+        mAgainstTimeText.setTypeface(dotBold);
+
+        mFirstTo100Text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, MainActivity.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                HomePage.this.startActivity(myIntent);
+            }
+        });
     }
 }
